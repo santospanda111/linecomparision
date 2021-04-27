9e7032ffa140dd62b32a66e129bac2997a51dc31
santospanda111 <santospanda111@gmail.com>
2021-04-27 07:00:20 +0530

    [santospanda111] added UC1 code

--- line.java
+++ line.java
@@ -1,5 +1,16 @@
+import java.util.*;
 public class Linecom {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first two co-ordinates:");
+		int x1=sc.nextInt();
+		int x2=sc.nextInt();
+		System.out.println("Enter second two co-ordinates:");
+		int y1=sc.nextInt();
+		int y2=sc.nextInt();
+		double length;
+		length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("Length of the line:" + length);
 	}
 }
